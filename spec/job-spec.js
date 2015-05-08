3173c47fb2f3176d9d3ab2c7fe637579d014b73f
Susan <susan.adelokiki@andela.co>
2015-05-08 12:06:24 +0100

    Refactor job spec

--- spec/job-spec.js
+++ spec/job-spec.js
@@ -2,25 +2,37 @@ require('../models/job.model');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/myDatabase');;
-Job = mongoose.model('Job');
+var Job = mongoose.model('Job'),
+myJob;
 
-//var 
-  
-
-
-  describe('Job model test', function(){
+describe('Job model test', function(){
 	 beforeEach(function(done){
-		myJob = new jobs();
+		myJob = new Job();
     done();
 	});
 
-	afterEach(function(done){
-		Job.remove().exec(function(){
-			done();
-		})
-	});
-	it('should be defined', function(done){
-		expect(Job).toBeDefined();
-		done();
-	});
+   describe('should not save if the title field is empty', function(){
+    it('should throw an error',function(done){
+      myJob.title = '';
+      myJob.save(function(error){
+        expect(error).toBeDefined();
+        console.log(error);
+        done();
+      });
+    });
+
+    it('should save the job', function(done){
+      myJob.title= "Andela Talent Accelerators";
+      myJob.details="Whatever",
+      myJob.location="Lagos",
+      myJob.date_posted= "date posted",
+      myJob.closing_date="closing date",
+      myJob.link="andela.co"
+
+      myJob.save(function(error){
+        expect(error).toBeNull();
+        done();
+      });
+    });
+  });
 });
