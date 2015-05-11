18e680aed488ce192087bd7a4a82c4a6df4d4e46
Susan <susan.adelokiki@andela.co>
2015-05-11 16:31:38 +0100

    refactor

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -71,6 +71,40 @@ User.findOne({firstname: req.body.firstname, lastname: req.body.lastname, email:
 });
 };
 exports.signIn = function(req, res){
-
+User.findOne({firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, username: req.body.username, mobilenumber:req.body.mobilenumber, password: req.body.password}, function(err, user) {
+  if (err) {
+    res.json({
+      type: false,
+      data: "Error occured: " + err
+  });
 }
+    else {
+    if (user) {
+    res.json({
+        type: false,
+        data: "User already exists!"
+    });
+    }
+else {
+  var userModel = new User();
+  userModel.firstname = req.body.firstname;
+  userModel.lastname = req.body.lastname;
+  userModel.email = req.body.email;
+  userModel.username = req.body.username;
+  userModel.mobilenumber = req.body.mobilenumber;
+  userModel.password = req.body.password;
+  userModel.save(function(err, user) {
+      user.token = jwt.sign(user, process.env.JWT_SECRET);
+      user.save(function(err, user1) {
+          res.json({
+              type: true,
+              data: user1,
+              token: user1.token
+          });
+      });
+  })
+}
+}
+});
+});
 
