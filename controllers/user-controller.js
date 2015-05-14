dd2ab56c53de23495992116ec7c6c9cd39288eb6
Susan <susan.adelokiki@andela.co>
2015-05-14 23:56:32 +0100

    Refactored user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -83,7 +83,7 @@ exports.verifyUser = function(req, res){
 
 
 exports.verifyToken = function(req, res){
-  var token = req.body.token || req.query.token || req.headers[x-access-token];
+  var token = req.body.token || req.query.token || req.headers["x-access-token"];
     if(token){
       jwt.verify(token, secret, function(err, decoded){
         if(err){
