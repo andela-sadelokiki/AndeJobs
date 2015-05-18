6bb240d8d730ce79b1e0592d78480767f4b7cc82
Susan <susan.adelokiki@andela.co>
2015-05-18 13:23:36 +0100

    Change date type

--- models/job.model.js
+++ models/job.model.js
@@ -4,7 +4,7 @@ var jobSchema = new Schema({
 	title: {type: String, required: true},
 	description:{type: String},
 	location: String,
-	date_posted: { type: String}
+	date_posted: { type: Date},
 });
 
 mongoose.model('Job', jobSchema);
