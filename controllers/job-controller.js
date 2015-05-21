5b2a0c67ed99d362fcb625013f981013764a0b59
Susan <susan.adelokiki@andela.co>
2015-05-21 01:45:14 +0100

    Implement apply, view Application and applicants functionality

--- controllers/job-controller.js
+++ controllers/job-controller.js
@@ -45,6 +45,46 @@ exports.deleteOneJob = function(req, res){
 		if(err){
 			res.json(err);
 		}
-		res.json({message: 'Job deleted!'});
+		res.json({job: job, message: 'Job deleted!'});
 	});
 };
+
+exports.apply = function(req, res){
+	Job.findById(req.params.job_id, function(err,job){
+		if(err){
+			res.json(err);
+		}
+		var applicants = job.appliedBy.addToSet(req.body.user_id);
+		if(!applicants.length){
+			res.status(400).json({message: "Multiple application not permitted"});
+		}
+		else{
+			job.save(function(err, job){
+				if(err){
+					res.json(err);
+				}
+				res.json(job);
+			});
+		}
+	});
+};
+
+exports.getApplications = function(req, res){
+	Job.findById(req.params.user_id).exec(function(err,job){
+		if(err){
+			return res.json(err);
+		}
+		res.json(job);
+	});
+};
+
+exports.getApplicants = function(req, res){
+	Job.findById(req.params.job_id, function(err, job){
+		if(err){
+			res.send(err);
+		}
+		res.send(job.appliedBy);
+	});
+};
+
+
