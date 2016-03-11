0b932f6c14d32757ede5fe4b916a88dbf73725af
Susan <susan.adelokiki@andela.co>
2016-03-11 15:56:16 +0100

    create new file

--- public/app/controllers/userctrl.js
+++ public/app/controllers/userctrl.js
@@ -1,11 +1,44 @@
-'use strict';
+  'use strict';
 
-angular.module('Andejobs')
-  .controller('UserCtrl', ['$scope', '$location', function($scope, $location) {
-    (function($) {
-      $(function() {
-        $('.parallax').parallax();
-        $('.scrollspy').scrollSpy();
-      });
-    })(jQuery);
-  }]);
+  angular.module('Andejobs')
+    .controller('UserCtrl', ['$scope', 'UserService', '$location', function($scope, UserService, $location) {
+      (function($) {
+        $(function() {
+          $('.parallax').parallax();
+          $('.scrollspy').scrollSpy();
+        });
+      })(jQuery);
+
+
+      $scope.saveToSessionStorage = function(id, token) {
+        $window.sessionStorage.token = token;
+        $window.sessionStorage.userId = id;
+      };
+
+      $scope.signup = function() {
+        UserService.register($scope.user).then(function(res) {
+          if (res.success === false) {
+            console.log("Registration failed");
+            $location.path('/');
+          } else {
+            console.log("u r registered");
+            $location.path('/dashboard');
+          }
+        }, function(err) {
+          console.log("an error occured", err);
+        });
+      };
+
+      $scope.signin = function() {
+        UserService.authenticate($scope.user).then(function(res) {
+          if (res.success === false) {
+            console.log("login failed");
+            $location.path('/')
+          } else {
+            $location.path('/dashboard');
+          }
+        }, function(err) {
+          console.log('err', err);
+        });
+      };
+    }]);
