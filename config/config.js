d7b102493c1369f0b6c6712516d1b75786022d77
Susan <susan.adelokiki@andela.co>
2016-03-11 15:52:51 +0100

    Create config file

--- config/config.js
+++ config/config.js
@@ -1,1 +1,7 @@
+"use strict";
+
+module.exports = {
+  'secret': 'susanspassword'
+  // 'database': 'mongodb://localhost:27017/dms-bk'
+};
 module.exports = require("./env/" + process.env.NODE_ENV + ".js");
