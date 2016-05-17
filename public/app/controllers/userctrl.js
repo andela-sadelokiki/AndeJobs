45a466db29733fc9167b88d2bcc0787fd085b719
Susan <susan.adelokiki@andela.co>
2016-05-17 14:47:36 +0100

    Refactor user controller

--- public/app/controllers/userctrl.js
+++ public/app/controllers/userctrl.js
@@ -1,44 +1,46 @@
   'use strict';
 
   angular.module('Andejobs')
-    .controller('UserCtrl', ['$scope', 'UserService', '$location', function($scope, UserService, $location) {
-      (function($) {
-        $(function() {
-          $('.parallax').parallax();
-          $('.scrollspy').scrollSpy();
-        });
-      })(jQuery);
+      .controller('UserCtrl', ['$scope', 'UserService', '$location', '$rootScope', function($scope, UserService, $location, $rootScope) {
+          (function($) {
+              $(function() {
+                  $('.parallax').parallax();
+                  $('.scrollspy').scrollSpy();
+              });
+          })(jQuery);
 
+          $scope.saveToSessionStorage = function(id, token) {
+            console.log('saving to session storage');
+              $window.sessionStorage.token = token;
+              $window.sessionStorage.userId = id;
+          };
 
-      $scope.saveToSessionStorage = function(id, token) {
-        $window.sessionStorage.token = token;
-        $window.sessionStorage.userId = id;
-      };
+          $scope.signup = function() {
+              UserService.register($scope.user).then(function(res) {
+                  if (res.success === false) {
+                      console.log("Registration failed");
+                      $location.path('/');
+                  } else {
+                      $rootScope.user = res.config.data;
+                      $location.path('/dashboard');
+                  }
+              }, function(err) {
+                  console.log("an error occured", err);
+              });
+          };
 
-      $scope.signup = function() {
-        UserService.register($scope.user).then(function(res) {
-          if (res.success === false) {
-            console.log("Registration failed");
-            $location.path('/');
-          } else {
-            console.log("u r registered");
-            $location.path('/dashboard');
-          }
-        }, function(err) {
-          console.log("an error occured", err);
-        });
-      };
-
-      $scope.signin = function() {
-        UserService.authenticate($scope.user).then(function(res) {
-          if (res.success === false) {
-            console.log("login failed");
-            $location.path('/')
-          } else {
-            $location.path('/dashboard');
-          }
-        }, function(err) {
-          console.log('err', err);
-        });
-      };
-    }]);
+          $scope.signin = function() {
+              UserService.authenticate($scope.user).then(function(res) {
+                  if (res.success === false) {
+                      console.log("login failed");
+                      $location.path('/')
+                  } else {
+                      $rootScope.user = res.config.data;
+                      $location.path('/dashboard');
+                      console.log(res, "you are logged in!");
+                  }
+              }, function(err) {
+                  console.log('err', err);
+              });
+          };
+      }]);
