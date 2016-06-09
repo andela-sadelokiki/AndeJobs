ab108a04553adb6b8cc908c423e6db872cfd19ae
Susan <susan.adelokiki@andela.co>
2016-06-09 13:54:17 +0100

    Refactor home controller

--- public/app/controllers/homectrl.js
+++ public/app/controllers/homectrl.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('Andejobs')
-  .controller('HomeCtrl', ['$scope', '$location', function($scope, $location) {
-    (function($) {
-      $(function() {
-        $('.parallax').parallax();
-        $('.scrollspy').scrollSpy();
-      });
-    })(jQuery);
-  }]);
+    .controller('HomeCtrl', ['$scope', '$location', function($scope, $location) {
+        (function($) {
+            $(function() {
+                $('.parallax').parallax();
+                $('.scrollspy').scrollSpy();
+            });
+        })(jQuery);
+    }]);
