9881c477c8793f7dea4ba04508aa47830ff5c25f
Susan <susan.adelokiki@andela.co>
2016-06-09 14:03:20 +0100

    method for modal in available jobs

--- public/app/controllers/jobctrl.js
+++ public/app/controllers/jobctrl.js
@@ -1,31 +1,31 @@
   'use strict';
 
   angular.module('Andejobs')
-      .controller('JobCtrl', ['$scope', 'JobService', '$location', function($scope, JobService, $location) {
+      .controller('JobCtrl', ['$scope', 'JobService', '$location', '$window', '$stateParams', function($scope, JobService, $location, $window, $stateParams) {
           (function($) {
               $(function() {
                   $('.modal-trigger').leanModal();
                   $('.parallax').parallax();
                   $('.scrollspy').scrollSpy();
-                  // $('#textarea1').val('New Text');
-                  // $('#textarea1').trigger('autoresize');
                   $('.modal-trigger').leanModal();
-
+                  $('.datepicker').pickadate({
+                      selectMonths: true,
+                      selectYears: 5
+                  });
               });
           })(jQuery);
 
+          $scope.alljobs = [];
           $scope.addJob = function() {
               JobService.post($scope.job).then(function(res) {
                   if (res.success === false) {
-                      console.log("failed to add new job!");
                       $location.path('/');
                   } else {
-                      console.log(res, "new job added successfully!");
-                      $location.path('/jobs')
+                      $scope.alljobs.push(res);
+                      $window.sessionStorage.jobId = res._id;
+                      $location.path('/jobs');
                   }
-              }, function(err) {
-                  console.log("an error occured", err);
-              });
+              }, function(err) {});
           };
 
           JobService.getAll().then(function(res) {
@@ -35,7 +35,10 @@
           $scope.apply = function(id) {
               $('#apply').openModal({
                   dismissible: true,
-                  opacity: .7
+                  opacity: 0.7
               });
+              JobService.getById(id).then(function(res) {
+                  $scope.jobInfo = res;
+              })
           };
       }]);
